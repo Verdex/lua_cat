31ced85f29b6f86a637b91f21fc1d8c82e4dccd9
David L. Inman <davidlevere@gmail.com>
2016-02-09 22:08:10 -0500

    minor comment change

--- compile.lua
+++ compile.lua
@@ -1,18 +1,19 @@
 
 
+-- input is an ast and the output is ... a list of instructions for the vm
 
 function blah( ast )
-    if ast.tag == "word" then
+    if ast.tag == "word" then 
 
-    elseif ast.tag == "string" then
+    elseif ast.tag == "string" then 
 
     elseif ast.tag == "number" then
 
-    elseif ast.tag == "lambda" then
+    elseif ast.tag == "lambda" then -- list of things that aren't definitions i think
 
-    elseif ast.tag == "top" then
+    elseif ast.tag == "top" then -- has like a list of top level stuff (comments and definitions?)
 
-    elseif ast.tag == "definition" then
+    elseif ast.tag == "definition" then -- name and then list of things that aren't definitions i think
 
     elseif ast.tag == "comment" then
 
