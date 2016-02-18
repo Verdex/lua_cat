5f7dcc19086b30bbcc519b6f1946ca93c85c092c
David L. Inman <davidlevere@gmail.com>
2016-02-17 21:28:56 -0500

    incremental progress

--- compile.lua
+++ compile.lua
@@ -1,6 +1,8 @@
 
 
 require 'seq'
+require 'vm_instr'
+require 'primitive'
 -- input is an ast and the output is ... a list of instructions for the vm
 
 
@@ -36,26 +38,71 @@ end
 
 -- take lambda ast node return an anonomous word def block (need gensym) AND a push word onto 
 -- stack opcode
-function process_lambda( lamb )
+-- node : lambda node 
+-- return : [ instr ], ???
+function process_lambda( node )
+    if node.tag ~= "lambda" then
+        error "compiler expected lambda node"
+    end
 
 end
 
 function process_list_of_things_not_def()
 end
 
+-- node : number node
+-- return : [ instr ] 
+function process_number( node )
+    if node.tag ~= "number" then
+        error "compiler expected number node"
+    end
+    return { { instr.push_number, node.value } } 
+end
 
+-- node : string node
+-- return : [ instr ]
+function process_string( node )
+    if node.tag ~= "string" then
+        error "compiler expected string node"
+    end
+    return { { instr.push_string, node.value } }
+end
+
+-- node : word node
+-- return : [ instr ] 
+function process_word( node )
+    if node.tag ~= "word" then
+        error "compiler expected word node"
+    end
+    if is_specal_word( node.value ) then
+        return process_special_word( node.value )
+    else
+        return { { instr.call_word, node.value } }
+    end
+end
 
 -- TODO I'm going to need a list of special words one way or the other (dup, etc)
 -- so I might as well add true and false there as well 
+function is_special_word( name ) 
+    return name == "dup" 
+        or name == "" 
+end
+
 function process_special_word()
+-- TODO grab the primitive word key from the primitive.lua and place it in index 2
+    local key = ''
+    if name == "dup" then
+        key = primvitive_key.dup
+    elseif true then
+
+    end
+
+    return { { instr.call_primitive, key }}
 end
 
 function blah( ast )
     if ast.tag == "word" then 
 
-    elseif ast.tag == "string" then 
-
-    elseif ast.tag == "number" then
 
     elseif ast.tag == "lambda" then
 
