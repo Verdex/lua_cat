6da8b68146b768468a4e2ed305c5a8ada9799b3e
David L. Inman <davidlevere@gmail.com>
2016-02-17 21:06:01 -0500

    object that will hold data in the running vm (like how i store strings etc)

---
+++ vm_obj.lua
@@ -0,0 +1,18 @@
+
+
+vm_obj_tag = {
+    word = "VM word type",
+    number = "VM number type",
+    string = "VM string type",
+    bool = "VM bool type",
+    primitive_word = "VM primitive word type",
+}
+
+local function copy( self )
+    return create_vm_obj( self.t, self.value )
+end
+
+
+function create_vm_obj( t, v )
+    return { tag = t, value = v, copy = copy }
+end
