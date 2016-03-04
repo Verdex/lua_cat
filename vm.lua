b354fd20f22a25d33683f97d830d07660ed74190
David L. Inman <davidlevere@gmail.com>
2016-03-03 23:05:31 -0500

    incremental progress

--- vm.lua
+++ vm.lua
@@ -4,57 +4,76 @@ require 'vm_obj'
 require 'primitive'
 require 'stack'
 
--- TODO need a vm object that has vm operations that we pass to primitive functions
-
 function run( instr_array )
 
     local data_stack = stack_create()
     local call_stack = stack_create()
     local ip = 1
+    local vm = { panic = panic }
 
     while ip <= #instr_array do
 
         local c_instr = instr_array[ ip ]
 
-        -- push current ip value on the call stack; jump to the address in index 2 
         if c_instr[1] == instr.call_word then
+            call_stack:push( ip )
+            ip = c_instr[2]
 
-        -- pop from data stack and check to see if it's tagged as a word
-        -- if it is tagged as a word then perform call word
-        -- else panic
         elseif c_instr[1] == instr.call_word_on_stack then 
+            local value_present, value = data_stack:pop()
+            if not value_present then
+                panic "no data on stack when trying to call word on stack"
+            end
+            if value.tag ~= vm_obj_tag.word then
+                panic "Attempting to call non word on stack"
+            end
+            call_stack:push( ip )
+            ip = value.value 
 
-        -- try to look up the lua function in index 2 (will be a key)
-        -- if the function can be found call it otherwise panic
         elseif c_instr[1] == instr.call_primitive then 
             local w = primitive_word[ c_instr[2] ]
             if w then
-                -- call word (not sure about params)
+                w( data_stack, vm )
             else
+                panic( "Unknown primitive word: " .. c_instr[2] )
+            end
+            ip = ip + 1
+
+        elseif c_instr[1] == instr.push_number then 
+            local v = create_vm_obj(vm_obj_tag.number, c_instr[2])
+            data_stack:push( v )
+            ip = ip + 1
+
+        elseif c_instr[1] == instr.push_string then 
+            local v = create_vm_obj(vm_obj_tag.string, c_instr[2])
+            data_stack:push( v )
+            ip = ip + 1
+       
+        elseif c_instr[1] == instr.push_word then 
+            local v = create_vm_obj(vm_obj_tag.word, c_instr[2])
+            data_stack:push( v )
+            ip = ip + 1
+
+        elseif c_instr[1] == instr.push_bool then 
+            local v = create_vm_obj(vm_obj_tag.bool, c_instr[2])
+            data_stack:push( v )
+            ip = ip + 1
+
+        elseif c_instr[1] == instr.ret then 
+            local value_present, value = call_stack:pop()
+            if not value_present then
                 -- panic
             end
-
-        -- tag the number in index 2 and push it onto the data stack
-        elseif c_instr[1] == instr.push_number then 
-
-        -- tag the string in index 2 and push it onto the data stack
-        elseif c_instr[1] == instr.push_string then 
-       
-        -- tag the word in index 2 and push it onto the data stack
-        elseif c_instr[1] == instr.push_word then 
-
-        -- tag the bool in index 2 and push it onto the data stack
-        elseif c_instr[1] == instr.push_bool then 
-
-        -- pop the call stack and set the value to ip
-        elseif c_instr[1] == instr.ret then 
+            ip = value
 
         else 
-            -- panic
+            panic( "Unknown instruction: " .. c_instr[1] )
         end
             
     end
 
 end
 
-
+loocal function panic( msg )
+    error( msg or "PANIC" )
+end
